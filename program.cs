7c50d48e10fc57a5ea207d48c1e33b2b62fa429f
NguyenBaTrungK215480106067 <k215480106067@tnut.edu.vn>
2024-09-10 01:39:54 +0700

    Update and rename ádf to program.cs

---
+++ program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp5
+     //Nguyễn Bá Trung
+     //bài toán về tính tiền nước đá
+
+  using System;
+
+namespace TinhTienNuocDa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Khai báo số lượng túi nước đá và giá mỗi túi
+            int soLuongTui = 75;
+            int giaMoiTui = 10000;
+
+            // Tính tổng số tiền
+            int tongSoTien = soLuongTui * giaMoiTui;
+
+            // In kết quả ra màn hình
+            Console.WriteLine("Tổng số tiền mà cửa hàng thu được từ việc bán nước đá là: " + tongSoTien + " VND");
+
+            // Đợi người dùng nhấn phím để đóng chương trình
+            Console.ReadKey();
+        }
+    }
+}
